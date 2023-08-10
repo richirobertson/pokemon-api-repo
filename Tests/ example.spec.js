ae980882b2eaeebca6968b131dd08d40965db15f
richirobertson <rich.robertson@gmail.com>
2023-08-10 12:18:07 +0100

    remove console log

--- Tests/ example.spec.js
+++ Tests/ example.spec.js
@@ -5,7 +5,6 @@ describe("Pokemon API Tests", function () {
         it("Knows Umbreon is the Best", async function () {
             const response = await request("https://pokeapi.co/api/v2")
                 .get("/pokemon/umbreon");
-            console.log(response.body)
             expect(response.body).to.have.property("stats");
             expect(response.body.stats).to.be.an("array");
             expect(response.body.stats).to.have.a.lengthOf(6);
