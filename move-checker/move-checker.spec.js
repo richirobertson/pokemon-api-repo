2019cfa6c6d95a2085d106ca8dbecc37a58a1fd1
richirobertson <rich.robertson292@gmail.com>
2023-12-17 09:31:55 +0000

    tidy up

---
+++ move-checker/move-checker.spec.js
@@ -0,0 +1,17 @@
+const { expect } = require("chai")
+const pokemonapi = require("../api/pokemonapi.js")
+
+const mon = "charmander"
+const attack = "flamethrower"
+
+describe("Move Checker", function () {
+    before(async function () {
+        response = await pokemonapi.pokemon(mon)
+    })
+    it(`${mon} can learn ${attack}!`, function () {
+        const move = response.body.moves.find(function (ability) {
+            return ability.move.name == attack;
+        })
+        expect(move).to.not.be.undefined;
+    })
+});
